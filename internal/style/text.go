5332645daad6bb318916a78fe1be381daae0c185
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-05-27 22:58:50 +0200

    add text style

---
+++ internal/style/text.go
@@ -0,0 +1,14 @@
+package style
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+var Normal = lipgloss.NewStyle().
+	Bold(false).
+	Foreground(lipgloss.Color("205"))
+
+var Selected = lipgloss.NewStyle().
+	Bold(false).
+	Foreground(lipgloss.Color("63")).
+	Background(lipgloss.Color("20"))
