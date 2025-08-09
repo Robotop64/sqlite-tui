bf314394031b2bf12c241c20797fdfed7f434834
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-08-09 03:00:12 +0200

    register available widgets and layout for the lua interpreter

--- internal/lua/widgets.go
+++ internal/lua/widgets.go
@@ -1,6 +1,38 @@
 package lua
 
+import lua "github.com/yuin/gopher-lua"
+
 type Widget interface {
-	SetData(data interface{})
-	SetActions(actions []interface{})
+	// SetData(data interface{})
+	// SetActions(actions []interface{})
+}
+
+
+func registerWidgets() {
+	registerWidget("WContainer")
+	registerWidget("LBox")
+	registerWidget("LFill")
+	registerWidget("LBBox")
+
+	registerWidget("WTable")
+	registerWidget("WView")
+	registerWidget("WButton")
+	registerWidget("WCheckList")
+	registerWidget("WLabel")
+}
+
+func registerWidget(name string) {
+	Env.SetGlobal(name, Env.NewFunction(func(L *lua.LState) int {
+		properties := L.CheckTable(1)
+
+		out := L.NewTable()
+		out.RawSetString("type", lua.LString(name))
+
+		properties.ForEach(func(k, v lua.LValue) {
+			out.RawSet(k, v)
+		})
+
+		L.Push(out)
+		return 1
+	}))
 }
