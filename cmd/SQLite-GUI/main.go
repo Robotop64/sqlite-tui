0329c3c8cc75b7a343ab24257d6291b7e0873355
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-08-15 03:35:08 +0200

    update package: main
    remove leftover, commented code

--- cmd/SQLite-GUI/main.go
+++ cmd/SQLite-GUI/main.go
@@ -1,39 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-
-// 	persistent "github.com/Robotop64/sqlite-tui/internal/persistent"
-// 	tabs "github.com/Robotop64/sqlite-tui/internal/tabs"
-
-// 	tea "github.com/charmbracelet/bubbletea"
-// )
-
-// func main() {
-// 	// load main config
-// 	if err := persistent.LoadConfig(); err != nil {
-// 		fmt.Println("Error loading config:", err)
-// 		os.Exit(1)
-// 	}
-// 	if err := persistent.LoadData(); err != nil {
-// 		fmt.Println("Error loading data:", err)
-// 		os.Exit(1)
-// 	}
-// 	// load profiles
-// 	persistent.LoadProfiles()
-
-// 	// initialize the core model
-// 	c := tabs.Init()
-
-// 	p := tea.NewProgram(c, tea.WithAltScreen(), tea.WithMouseAllMotion())
-
-// 	if _, err := p.Run(); err != nil {
-// 		fmt.Println("Error running program:", err)
-// 		os.Exit(1)
-// 	}
-// }
-
 package main
 
 import (
@@ -53,11 +17,12 @@ func main() {
 }
 
 func setup() {
-	// load main config
+	// load config
 	if err := Persistent.LoadConfig(); err != nil {
 		fmt.Println("Error loading config:", err)
 		os.Exit(1)
 	}
+	// load user data
 	if err := Persistent.LoadData(); err != nil {
 		fmt.Println("Error loading data:", err)
 		os.Exit(1)
