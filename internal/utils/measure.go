ffd6ba823eb5a9ac17d9384c802cd86db57f3c41
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-08-15 03:33:27 +0200

    update package: utils - measure
    * make Max(), Min() and Dimension{} generic, narrowed to int and float
    + FitSize() to create new Dimensio{} fitting & expanding to a specified region

--- internal/utils/measure.go
+++ internal/utils/measure.go
@@ -1,20 +1,39 @@
 package utils
 
-func Max(a, b int) int {
+import "fyne.io/fyne/v2"
+
+func Max[T ~int | ~float32 | ~float64](a, b T) T {
 	if a > b {
 		return a
 	}
 	return b
 }
 
-func Min(a, b int) int {
+func Min[T ~int | ~float32 | ~float64](a, b T) T {
 	if a < b {
 		return a
 	}
 	return b
 }
 
-type Dimensions struct {
-	Width  int
-	Height int
+type Dimensions[T ~int | ~float32 | ~float64] struct {
+	Width  T
+	Height T
+}
+
+func (d Dimensions[T]) ToFyneSize() fyne.Size {
+	return fyne.NewSize(float32(d.Width), float32(d.Height))
+}
+func FyneToDimensions(size fyne.Size) Dimensions[float32] {
+	return Dimensions[float32]{
+		Width:  float32(size.Width),
+		Height: float32(size.Height),
+	}
+}
+
+func FitSize[T ~int | ~float32 | ~float64](item, container Dimensions[T]) Dimensions[T] {
+	return Dimensions[T]{
+		Width:  Min(item.Width, container.Width),
+		Height: Min(item.Height, container.Height),
+	}
 }
