b56d382e19dddb3f3c6e447c0193ecd48e949f4f
David Capalbo <capalbo.david@gmail.com>
2025-08-17 01:53:27 +0200

    update package: lua - engine
    * separate LoadView -> LoadScript + LoadView: separattion for loading and rendering
    + LoadSources

--- internal/lua/engine.go
+++ internal/lua/engine.go
@@ -37,14 +37,17 @@ func Clean() {
 	}
 }
 
-func LoadView(script persistent.Script) (fyne.CanvasObject, error) {
-
+func LoadScript(script persistent.Script) error {
+	Env.SetGlobal("sources", lua.LNil)
 	Env.SetGlobal("layout", lua.LNil)
 
 	if err := Env.DoString(string(script.Script)); err != nil {
-		return FWidget.NewLabel(fmt.Sprintf("Failed to load the script of the selected view.\nCaused by error:\n\t %v", err)), fmt.Errorf("failed to load Lua script: %w", err)
+		return fmt.Errorf("failed to load Lua script: %w", err)
 	}
+	return nil
+}
 
+func LoadView() (fyne.CanvasObject, error) {
 	var lua_layout *lua.LTable
 
 	if layout := Env.GetGlobal("layout"); layout.Type() == lua.LTTable {
@@ -54,4 +57,27 @@ func LoadView(script persistent.Script) (fyne.CanvasObject, error) {
 	}
 
 	return buildLayout(Env, lua_layout), nil
+}
+
+func LoadSources() error {
+	persistent.Sources = make([]*persistent.Source, 0)
+
+	var lua_sources *lua.LTable
+	if sources := Env.GetGlobal("sources"); sources.Type() == lua.LTTable {
+		lua_sources = sources.(*lua.LTable)
+	} else {
+		return fmt.Errorf("sources not found in Lua script")
+	}
+
+	for i := 1; i <= lua_sources.Len(); i++ {
+		target := persistent.ActiveTarget()
+		idx_source := int(lua.LVAsNumber(lua_sources.RawGetInt(i)))
+		if _, err := persistent.NewSource(target.SourcePaths[idx_source]); err != nil {
+			return fmt.Errorf("failed to create source from path '%s': %w", target.SourcePaths[idx_source-1], err)
+		}
+	}
+
+	fmt.Printf("Loaded %d source(s) from Lua script.\n", len(persistent.Sources))
+
+	return nil
 }
